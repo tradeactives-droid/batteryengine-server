d9b8f6b9ef6c245113a32bdf2e33c0524e7b42be
tradeactives-droid <tradeactives@gmail.com>
2025-12-12 20:29:59 +0100

    Update scenario_runner.py

--- battery_engine_pro3/scenario_runner.py
+++ battery_engine_pro3/scenario_runner.py
@@ -1,17 +1,21 @@
-# battery_engine_pro3/scenario_runner.py
-
+from __future__ import annotations
 from dataclasses import dataclass
-from typing import Dict
+from typing import Dict, List
+
+from .types import TimeSeries, TariffConfig, BatteryConfig, ScenarioResult, PeakInfo
+from .battery_model import BatteryModel
 from .battery_simulator import BatterySimulator
 from .cost_engine import CostEngine
 from .peak_optimizer import PeakOptimizer
-from .types import ScenarioResult
 
 
 @dataclass
-class PeakInfo:
-    monthly_before: list
-    monthly_after: list
+class FullScenarioOutput:
+    A1: ScenarioResult
+    B1: Dict[str, ScenarioResult]
+    C1: Dict[str, ScenarioResult]
+    roi: float
+    peaks: PeakInfo
 
 
 class ScenarioRunner:
@@ -22,17 +26,34 @@ class ScenarioRunner:
         self.tariff_cfg = tariff_cfg
         self.batt_cfg = batt_cfg
 
-    def run(self):
-        sim = BatterySimulator(self.load, self.pv, None)
-        base = sim.simulate_no_battery()
-        cost_engine = CostEngine(self.tariff_cfg)
+    def run(self) -> FullScenarioOutput:
+        cost = CostEngine(self.tariff_cfg)
 
-        A1 = cost_engine.compute_cost(base.import_profile, base.export_profile, self.tariff_cfg.current_tariff)
+        sim_no = BatterySimulator(self.load, self.pv, None).simulate_no_battery()
+        A1 = cost.compute_cost(sim_no.import_profile, sim_no.export_profile, self.tariff_cfg.current_tariff)
 
-        return {
-            "A1": A1,
-            "B1": A1,
-            "C1": A1,
-            "roi": 0.0,
-            "peaks": PeakInfo([0]*12, [0]*12)
+        B1 = {
+            t: cost.compute_cost(sim_no.import_profile, sim_no.export_profile, t)
+            for t in ["enkel", "dag_nacht", "dynamisch"]
         }
+
+        battery = BatteryModel(
+            self.batt_cfg.E,
+            self.batt_cfg.P,
+            self.batt_cfg.DoD,
+            self.batt_cfg.eta_rt
+        )
+
+        sim_batt = BatterySimulator(self.load, self.pv, battery).simulate_with_battery()
+
+        C1 = {
+            t: cost.compute_cost(sim_batt.import_profile, sim_batt.export_profile, t)
+            for t in ["enkel", "dag_nacht", "dynamisch"]
+        }
+
+        peaks = PeakInfo([], [])
+
+        roi = (B1[self.tariff_cfg.current_tariff].total_cost_eur -
+               C1[self.tariff_cfg.current_tariff].total_cost_eur) / self.batt_cfg.investment_eur
+
+        return FullScenarioOutput(A1, B1, C1, roi, peaks)
