5afb197542950a61a62f52a4963982b499a41315
tradeactives-droid <tradeactives@gmail.com>
2025-12-12 20:29:19 +0100

    Update peak_optimizer.py

--- battery_engine_pro3/peak_optimizer.py
+++ battery_engine_pro3/peak_optimizer.py
@@ -1,45 +1,44 @@
-# battery_engine_pro3/peak_optimizer.py
-
-from typing import List, Tuple
-from .battery_model import BatteryModel
+from __future__ import annotations
+from typing import List
 from .types import TimeSeries
+from .battery_model import BatteryModel
 
 
 class PeakOptimizer:
 
     @staticmethod
     def compute_monthly_peaks(load: TimeSeries, pv: TimeSeries) -> List[float]:
-        peaks = [0.0]*12
-        for l, p, t in zip(load.values, pv.values, load.timestamps):
-            peaks[t.month-1] = max(peaks[t.month-1], max(0, l-p))
+        peaks = [0.0] * 12
+        for t, l, p in zip(load.timestamps, load.values, pv.values):
+            net = max(0.0, l - p)
+            m = t.month - 1
+            peaks[m] = max(peaks[m], net)
         return peaks
 
     @staticmethod
-    def compute_monthly_targets(peaks, reduction_factor=0.85):
-        return [p * reduction_factor for p in peaks]
+    def compute_monthly_targets(baseline: List[float], reduction_factor: float = 0.85):
+        return [p * reduction_factor for p in baseline]
 
     @staticmethod
-    def simulate_with_peak_shaving(load, pv, battery, targets, soc_plan=None):
-        # 🔑 test vraagt deze signature
-        soc_plan = soc_plan or [battery.E_min]*len(load.values)
+    def simulate_with_peak_shaving(load, pv, battery: BatteryModel, targets):
+        imp, exp, soc = [], [], []
+        soc_kwh = battery.initial_soc_kwh
+        peaks = [0.0] * 12
 
-        import_p, export_p, soc_p = [], [], []
-        soc = battery.initial_soc_kwh
-        dt = load.dt_hours
-        monthly_after = [0.0]*12
-
-        for l, p, t, smin in zip(load.values, pv.values, load.timestamps, soc_plan):
+        for t, l, p in zip(load.timestamps, load.values, pv.values):
+            m = t.month - 1
             net = l - p
-            if net > targets[t.month-1]:
-                needed = net - targets[t.month-1]
-                discharge = min(needed, battery.power_kw)
-                energy = min(discharge*dt, soc - smin)
-                soc -= energy
-                import_p.append(net - energy/dt)
-            else:
-                import_p.append(max(0, net))
-            export_p.append(max(0, -net))
-            soc_p.append(soc)
-            monthly_after[t.month-1] = max(monthly_after[t.month-1], import_p[-1])
 
-        return monthly_after, import_p, export_p, soc_p
+            if net > targets[m]:
+                shave = min(net - targets[m], battery.P_max)
+                shave_kwh = shave / battery.eta_discharge
+                shave_kwh = min(shave_kwh, soc_kwh - battery.E_min)
+                soc_kwh -= shave_kwh
+                net -= shave_kwh * battery.eta_discharge
+
+            imp.append(max(0.0, net))
+            exp.append(max(0.0, -net))
+            soc.append(soc_kwh)
+            peaks[m] = max(peaks[m], imp[-1])
+
+        return peaks, imp, exp, soc
