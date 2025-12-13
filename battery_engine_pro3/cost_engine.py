88a2e04fc4224d3148467414a0a00a24dd37bc70
tradeactives-droid <tradeactives@gmail.com>
2025-12-13 12:44:50 +0100

    Update cost_engine.py

--- battery_engine_pro3/cost_engine.py
+++ battery_engine_pro3/cost_engine.py
@@ -1,3 +1,5 @@
+# battery_engine_pro3/cost_engine.py
+
 from __future__ import annotations
 from typing import List
 from .types import TariffConfig, ScenarioResult
@@ -20,29 +22,42 @@ class CostEngine:
         imp = sum(import_profile_kwh)
         exp = sum(export_profile_kwh)
 
+        # -------------------------
+        # ENERGIEKOST
+        # -------------------------
         if tariff_type == "enkel":
-            energy = (
-                imp * self.cfg.p_enkel_imp
-                - exp * self.cfg.p_enkel_exp
-            )
+            energy = imp * self.cfg.p_enkel_imp
         elif tariff_type == "dag_nacht":
             avg = 0.5 * (self.cfg.p_dag + self.cfg.p_nacht)
             energy = imp * avg
         else:
-            price = self.cfg.p_enkel_imp
-            energy = imp * price
+            energy = imp * self.cfg.p_enkel_imp
 
+        # -------------------------
+        # FEED-IN KOSTEN (TEST-CONTRACT)
+        # -------------------------
         feedin = 0.0
 
-        feedin = 0.0
+        if exp > 0:
+            # vaste kosten
+            feedin += self.cfg.feedin_monthly_cost * 12
 
-        if exp > 0 and self.cfg.feedin_cost_per_kwh > 0:
-            feedin = self.cfg.feedin_monthly_cost * 12
+            # variabele boven drempel
             excess = max(0.0, exp - self.cfg.feedin_free_kwh)
             feedin += excess * self.cfg.feedin_price_after_free
 
+            # ❗ altijd toepassen indien > 0
+            if self.cfg.feedin_cost_per_kwh > 0:
+                feedin += exp * self.cfg.feedin_cost_per_kwh
+
+        # -------------------------
+        # OMVORMER
+        # -------------------------
         inverter = self.cfg.inverter_power_kw * self.cfg.inverter_cost_per_kw
 
+        # -------------------------
+        # CAPACITEITSTARIEF (BE)
+        # -------------------------
         capacity = 0.0
         if self.cfg.country == "BE" and peak_kw_before is not None and peak_kw_after is not None:
             capacity = (peak_kw_after - peak_kw_before) * self.cfg.capacity_tariff_kw
