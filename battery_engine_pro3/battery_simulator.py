26c4704134e9209987c2a0e318885d018fefca5d
tradeactives-droid <tradeactives@gmail.com>
2025-12-13 19:36:52 +0100

    Update battery_simulator.py

--- battery_engine_pro3/battery_simulator.py
+++ battery_engine_pro3/battery_simulator.py
@@ -55,7 +55,7 @@ class BatterySimulator:
             return self.simulate_no_battery()
 
         batt = self.battery
-        soc = batt.initial_soc_kwh
+        soc = 0.0
 
         import_p = []
         export_p = []
