f3b7e6392a92fef7455ec5256a3dc14134fa8488
Maikel Kuppens <tradeactives@gmail.com>
2026-03-29 16:28:43 +0200

    payment

--- main.py
+++ main.py
@@ -1569,14 +1569,6 @@ def stripe_create_checkout_session(req: StripeCheckoutSessionRequest):
                 "bancontact",
                 "sepa_debit",
             ],
-            payment_method_options={
-                "ideal": {
-                    "setup_future_usage": "off_session",
-                },
-                "sepa_debit": {
-                    "setup_future_usage": "off_session",
-                },
-            },
             billing_address_collection="auto",
             mode="subscription",
             line_items=[
